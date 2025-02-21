1cf53c8d2d4cc9a4897d544ced952c1309882a55
GayatriAvhad <145040660+GayatriAvhad@users.noreply.github.com>
2025-02-21 20:18:18 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,16 @@
+
+#create database
+create database category_product_db;
+
+#use this database
+use category_product_db;
+
+drop table categories;
+
+create database categoryproductdb;
+use categoryproductdb;
+
+create database apidb;
+
+drop database category_product_db;
+
